2c99527e3d8f579f96c10d13cf89fba5e4458d2e
Michael <yidongnan@gmail.com>
2016-08-15 16:58:41 +0800

    * prepare use docker

---
+++ service-a/src/main/java/net/devh/AServiceApplication.java
@@ -0,0 +1,24 @@
+package net.devh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+/**
+ * User: Michael
+ * Email: zhangyichen@xiaoniu66.com
+ * Date: 2016/6/3
+ */
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableFeignClients
+@SpringBootApplication
+public class AServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AServiceApplication.class, args);
+    }
+}
