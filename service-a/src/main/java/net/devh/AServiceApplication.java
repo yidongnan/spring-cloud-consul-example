4421f3a7e2fbc093da901adaeb18015ca9bc8187
Michael Zhang <yidongnan@gmail.com>
2016-11-07 22:53:29 +0800

    * modify email address

--- service-a/src/main/java/net/devh/AServiceApplication.java
+++ service-a/src/main/java/net/devh/AServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * User: Michael
- * Email: zhangyichen@xiaoniu66.com
+ * Email: yidongnan@gmail.com
  * Date: 2016/6/3
  */
 @EnableDiscoveryClient
--- service-b/src/main/java/net/devh/BServiceApplication.java
+++ service-b/src/main/java/net/devh/BServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * User: Michael
- * Email: zhangyichen@xiaoniu66.com
+ * Email: yidongnan@gmail.com
  * Date: 2016/6/3
  */
 @SpringBootApplication
