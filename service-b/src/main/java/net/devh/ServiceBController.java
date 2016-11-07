9dd7f62b46c83be170ce3a19b265b3e8c05cb032
Michael Zhang <yidongnan@gmail.com>
2016-11-07 22:52:06 +0800

    + add RefreshScope annotation

--- service-b/src/main/java/net/devh/ServiceBController.java
+++ service-b/src/main/java/net/devh/ServiceBController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Email: zhangyichen@xiaoniu66.com
  * Date: 2016/6/3
  */
+@RefreshScope
 @RestController
 public class ServiceBController {
 
