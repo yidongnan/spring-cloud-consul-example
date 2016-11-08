4501f7da0440b46205fb724a31208a32ed21a0bb
Michael Zhang <yidongnan@gmail.com>
2016-11-08 10:32:08 +0800

    * support docker

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 modules=( admin-dashboard hystrix-dashboard service-a service-b zuul )
 
 for module in "${modules[@]}"; do
-    docker build -t "spring-cloud-netflix-example/${module}:latest" ${module}
+    docker build -t "spring-cloud-consul-example/${module}:latest" ${module}
 done
